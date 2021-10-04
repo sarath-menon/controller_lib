87d80f0e66d25727790b35b384c7b4c52085c4e4
smenon <smenon@ethz.ch>
2021-10-04 22:40:16 +0200

    added basic 3d quad mixer

--- quadcopter_mixers/saturation_cutoff_mixer/include/saturation_cutoff_mixer.h
+++ quadcopter_mixers/saturation_cutoff_mixer/include/saturation_cutoff_mixer.h
@@ -9,18 +9,37 @@ class QuadcopterMixer {
 
 private:
   // Quadcopter Properties
-  float arm_length = 0;
-  float k_f = 0;
+  float arm_length = 0.171;
+
+  float k_f = 6.11e-8;
+  float k_t = 1.5e-9;
+
+  // Individual motor thrust max, min
   float motor_thrust_max = 0;
   float motor_thrust_min = 0;
+
+  // Quadcopter net thrust max, min
+  float thrust_max = 27;
+  float thrust_min = 7;
 
 public:
   // Mixer
   // void motor_mixer(float motor_commands[4], const float thrust_command,
   //                  const float torque_command);
   // Mixer
-  matrix::Vector<float, 4> motor_mixer(const float thrust_command,
-                                       const float torque_command);
+
+  // Initialize mixer matrix
+  QuadcopterMixer();
+
+  // Mixer
+  matrix::Vector<float, 4>
+  motor_mixer(matrix::Vector<float, 4> thrust_torque_command);
+
+  // Vector of motor commands
+  matrix::Vector<float, 4> motor_commands;
+
+  // Mixer Matrix
+  matrix::SquareMatrix<float, 4> mixer_matrix_;
 
 public:
   // To load quadcopter properties from yaml file
---
+++ quadcopter_mixers/saturation_cutoff_mixer/src/saturation_cutoff_mixer.cpp
@@ -0,0 +1,54 @@
+#include "saturation_cutoff_mixer.h"
+#include <iostream>
+
+QuadcopterMixer::QuadcopterMixer() {
+  mixer_matrix_(0, 0) = 0.25;
+  mixer_matrix_(0, 1) = 0.0;
+  mixer_matrix_(0, 2) = 0.5 / arm_length;
+  mixer_matrix_(0, 3) = 0.25 / k_t;
+
+  mixer_matrix_(1, 0) = 0.25;
+  mixer_matrix_(1, 1) = 0.5 / arm_length;
+  mixer_matrix_(1, 2) = 0.0;
+  mixer_matrix_(1, 3) = -0.25 / k_t;
+
+  mixer_matrix_(2, 0) = 0.25;
+  mixer_matrix_(2, 1) = 0.0;
+  mixer_matrix_(2, 2) = -0.5 / arm_length;
+  mixer_matrix_(2, 3) = 0.25 / k_t;
+
+  mixer_matrix_(3, 0) = 0.25;
+  mixer_matrix_(3, 1) = -0.5 / arm_length;
+  mixer_matrix_(3, 2) = 0.0;
+  mixer_matrix_(3, 3) = -0.25 / k_t;
+}
+
+matrix::Vector<float, 4>
+QuadcopterMixer::motor_mixer(matrix::Vector<float, 4> thrust_torque_command) {
+
+  matrix::Vector<float, 4> motor_thrusts =
+      mixer_matrix_ * thrust_torque_command;
+
+  for (int i = 0; i < 4; i++) {
+    motor_thrusts(i) = math_helper::limit(motor_thrusts(i), motor_thrust_max,
+                                          motor_thrust_min);
+    motor_commands(i) = sqrt(motor_thrusts(i) / k_f);
+  }
+
+  // std::cout << "Actual Motor 1 command " << motor_commands[0] <<
+  // std::endl;
+
+  //   std::cout << "Controller: f1:" << f1 << "\tf2:" << f2 << "\tf3:" <<
+  //   f3
+  //             << "\tf4:" << f4 << std::endl;
+  // std::cout << "Net thrust and torque before  motor mixing:"
+  //           << thrust_torque_command(0) << '\t' << thrust_torque_command(1)
+  //           << std::endl;
+  // std::cout << "Net thrust and torque after motor mixing:"
+  //           << motor_thrusts(0) + motor_thrusts(1) + motor_thrusts(2) +
+  //                  motor_thrusts(3)
+  //           << '\t' << (motor_thrusts(1) - motor_thrusts(3)) * arm_length
+  //           << std::endl;
+
+  return motor_commands;
+}
